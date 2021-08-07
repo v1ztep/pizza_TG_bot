0861a930b940cc1b3f091528e49314941b3849fb
v1ztep <v1ztep@gmail.com>
2021-08-07 03:28:09 +0300

    Изменил получение токена и вынес в отдельный модуль загрузку меню

--- moltin.py
+++ moltin.py
@@ -1,30 +1,30 @@
-import os
 import time
-import json
+
 import requests
-from dotenv import load_dotenv
 from slugify import slugify
 
 EP_ACCESS_TOKEN = None
 EP_TOKEN_LIFETIME = None
 
 
-def get_ep_access_token(moltin_token):
+def get_ep_access_token(moltin_token, moltin_secret):
     now_time = time.time()
     global EP_ACCESS_TOKEN
     global EP_TOKEN_LIFETIME
 
     if not EP_TOKEN_LIFETIME or EP_TOKEN_LIFETIME < now_time:
-        EP_ACCESS_TOKEN, EP_TOKEN_LIFETIME = create_ep_access_token(moltin_token)
+        EP_ACCESS_TOKEN, EP_TOKEN_LIFETIME = create_ep_access_token(moltin_token,
+                                                                    moltin_secret)
 
     return EP_ACCESS_TOKEN
 
 
-def create_ep_access_token(moltin_token):
+def create_ep_access_token(moltin_token, moltin_secret):
     url = 'https://api.moltin.com/oauth/access_token'
     data = {
         'client_id': moltin_token,
-        'grant_type': 'implicit'
+        'client_secret': moltin_secret,
+        'grant_type': 'client_credentials'
     }
     response = requests.post(url, data=data)
     response.raise_for_status()
@@ -34,8 +34,8 @@ def create_ep_access_token(moltin_token):
     return ep_access_token, ep_token_lifetime
 
 
-def get_all_products(moltin_token):
-    access_token = get_ep_access_token(moltin_token)
+def get_all_products(moltin_token, moltin_secret):
+    access_token = get_ep_access_token(moltin_token, moltin_secret)
     headers = {
         'Authorization': f'Bearer {access_token}',
     }
@@ -45,8 +45,8 @@ def get_all_products(moltin_token):
     return response.json()
 
 
-def get_product(moltin_token, product_id):
-    access_token = get_ep_access_token(moltin_token)
+def get_product(moltin_token, moltin_secret, product_id):
+    access_token = get_ep_access_token(moltin_token, moltin_secret)
     headers = {
         'Authorization': f'Bearer {access_token}',
     }
@@ -56,8 +56,8 @@ def get_product(moltin_token, product_id):
     return response.json()
 
 
-def get_image(moltin_token, image_id):
-    access_token = get_ep_access_token(moltin_token)
+def get_image(moltin_token, moltin_secret, image_id):
+    access_token = get_ep_access_token(moltin_token, moltin_secret)
     headers = {
         'Authorization': f'Bearer {access_token}',
     }
@@ -68,9 +68,9 @@ def get_image(moltin_token, image_id):
     return image
 
 
-def add_product_to_cart(moltin_token, cart_id, product_id, quantity):
+def add_product_to_cart(moltin_token, moltin_secret, cart_id, product_id, quantity):
     url = f'https://api.moltin.com/v2/carts/{cart_id}/items'
-    access_token = get_ep_access_token(moltin_token)
+    access_token = get_ep_access_token(moltin_token, moltin_secret)
     headers = {
         'Authorization': f'Bearer {access_token}',
         'Content-Type': 'application/json',
@@ -85,8 +85,8 @@ def add_product_to_cart(moltin_token, cart_id, product_id, quantity):
     return response.json()
 
 
-def get_cart(moltin_token, cart_id):
-    access_token = get_ep_access_token(moltin_token)
+def get_cart(moltin_token, moltin_secret, cart_id):
+    access_token = get_ep_access_token(moltin_token, moltin_secret)
     headers = {
         'Authorization': f'Bearer {access_token}',
     }
@@ -96,9 +96,9 @@ def get_cart(moltin_token, cart_id):
     return response.json()
 
 
-def get_cart_items(moltin_token, cart_id):
+def get_cart_items(moltin_token, moltin_secret, cart_id):
     url = f'https://api.moltin.com/v2/carts/{cart_id}/items'
-    access_token = get_ep_access_token(moltin_token)
+    access_token = get_ep_access_token(moltin_token, moltin_secret)
     headers = {
         'Authorization': f'Bearer {access_token}',
     }
@@ -107,9 +107,9 @@ def get_cart_items(moltin_token, cart_id):
     return response.json()
 
 
-def remove_item_in_cart(moltin_token, cart_id, cart_item_id):
+def remove_item_in_cart(moltin_token, moltin_secret, cart_id, cart_item_id):
     url = f'https://api.moltin.com/v2/carts/{cart_id}/items/{cart_item_id}'
-    access_token = get_ep_access_token(moltin_token)
+    access_token = get_ep_access_token(moltin_token, moltin_secret)
     headers = {
         'Authorization': f'Bearer {access_token}',
     }
@@ -118,9 +118,9 @@ def remove_item_in_cart(moltin_token, cart_id, cart_item_id):
     return response.json()
 
 
-def create_customer(moltin_token, name, email):
+def create_customer(moltin_token, moltin_secret, name, email):
     url = f'https://api.moltin.com/v2/customers'
-    access_token = get_ep_access_token(moltin_token)
+    access_token = get_ep_access_token(moltin_token, moltin_secret)
     headers = {
         'Authorization': f'Bearer {access_token}',
         'Content-Type': 'application/json',
@@ -135,9 +135,9 @@ def create_customer(moltin_token, name, email):
     return response.json()
 
 
-def get_customer(moltin_token, user_id):
+def get_customer(moltin_token, moltin_secret, user_id):
     url = f'https://api.moltin.com/v2/customers/{user_id}'
-    access_token = get_ep_access_token(moltin_token)
+    access_token = get_ep_access_token(moltin_token, moltin_secret)
     headers = {
         'Authorization': f'Bearer {access_token}',
     }
@@ -146,17 +146,17 @@ def get_customer(moltin_token, user_id):
     return response.json()
 
 
-def create_product(moltin_token, name, sku, description, price):
-    access_token = get_ep_access_token(moltin_token)
+def create_product(moltin_token, moltin_secret, name, sku, description, price):
+    access_token = get_ep_access_token(moltin_token, moltin_secret)
     headers = {
         'Authorization': f'Bearer {access_token}',
         'Content-Type': 'application/json',
     }
     data = {'data':{
-        'type': 'pizza',
+        'type': 'product',
         'name': name,
         'slug': slugify(name),
-        'sku': sku,
+        'sku': str(sku),
         'description': description,
         'manage_stock': False,
         'price': [{
@@ -169,28 +169,26 @@ def create_product(moltin_token, name, sku, description, price):
     }}
     response = requests.post('https://api.moltin.com/v2/products',
                             headers=headers, json=data)
-    print(response.text)
     response.raise_for_status()
     return response.json()
 
 
-def create_file(moltin_token, image_url):
-    access_token = get_ep_access_token(moltin_token)
+def create_file(moltin_token, moltin_secret, image_url):
+    access_token = get_ep_access_token(moltin_token, moltin_secret)
     headers = {
-        'Authorization': f'Bearer {access_token}',
+        'Authorization': f'Bearer {access_token}'
     }
     files = {
-        'file-location': (None, image_url),
+        'file_location': (None, image_url)
     }
     response = requests.post('https://api.moltin.com/v2/files',
                              headers=headers, files=files)
-    print(response.text)
     response.raise_for_status()
     return response.json()
 
 
-def create_image_relationship(moltin_token, product_id, image_id):
-    access_token = get_ep_access_token(moltin_token)
+def create_image_relationship(moltin_token, moltin_secret, product_id, image_id):
+    access_token = get_ep_access_token(moltin_token, moltin_secret)
     headers = {
         'Authorization': f'Bearer {access_token}',
         'Content-Type': 'application/json',
@@ -201,34 +199,6 @@ def create_image_relationship(moltin_token, product_id, image_id):
     }}
     response = requests.post(
         f'https://api.moltin.com/v2/products/{product_id}/relationships/main-image',
-        headers=headers, data=data)
-    print(response.text)
+        headers=headers, json=data)
     response.raise_for_status()
     return response.json()
-
-
-def main():
-    load_dotenv()
-    moltin_token = os.getenv('ELASTICPATH_CLIENT_ID')
-
-    with open("menu.json", "r", encoding="utf8") as file:
-        menu_json = file.read()
-    menu = json.loads(menu_json)
-
-    for pizza in menu:
-        name = pizza['name']
-        sku = pizza['id']
-        description = pizza['description']
-        price = pizza['price']
-        image_url = pizza['product_image']['url']
-        product = create_product(moltin_token, name, sku, description, price)
-        product_id = product['data']['id']
-        image = create_file(moltin_token, image_url)
-        image_id = image['data']['id']
-        create_image_relationship(moltin_token, product_id, image_id)
-        break
-
-
-
-if __name__ == '__main__':
-    main()
