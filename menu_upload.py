44cfbeda61d92265506de8f694b22267b0d71b67
v1ztep <v1ztep@gmail.com>
2021-08-07 03:28:51 +0300

    скрипт на загрузку меню в CMS

---
+++ menu_upload.py
@@ -0,0 +1,32 @@
+import json
+import os
+
+from dotenv import load_dotenv
+
+from moltin import create_file
+from moltin import create_image_relationship
+from moltin import create_product
+
+load_dotenv()
+
+
+moltin_token = os.getenv('ELASTICPATH_CLIENT_ID')
+moltin_secret = os.getenv('ELASTICPATH_CLIENT_SECRET')
+
+with open("menu.json", "r", encoding="utf8") as file:
+    menu_json = file.read()
+menu = json.loads(menu_json)
+
+for pizza in menu:
+    name = pizza['name']
+    sku = pizza['id']
+    description = pizza['description']
+    price = pizza['price']
+    image_url = pizza['product_image']['url']
+    product = create_product(moltin_token, moltin_secret, name,
+                             sku, description, price)
+    product_id = product['data']['id']
+    image = create_file(moltin_token, moltin_secret, image_url)
+    image_id = image['data']['id']
+    create_image_relationship(moltin_token, moltin_secret,
+                              product_id, image_id)
