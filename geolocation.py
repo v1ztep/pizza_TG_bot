5eaa1cad084c17915dbbedca352ef29c039c531d
v1ztep <v1ztep@gmail.com>
2021-08-24 21:08:09 +0300

    Убрал отладочный принт

--- geolocation.py
+++ geolocation.py
@@ -5,7 +5,6 @@ def fetch_coordinates(apikey, place):
     base_url = "https://geocode-maps.yandex.ru/1.x"
     params = {"geocode": place, "apikey": apikey, "format": "json"}
     response = requests.get(base_url, params=params)
-    print(response.text)
     response.raise_for_status()
     found_places = response.json()['response']['GeoObjectCollection']['featureMember']
     if not found_places:
