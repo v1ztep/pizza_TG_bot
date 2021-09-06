fe2b54a1bc95a8109db7883a25af4b3d02b58bf9
v1ztep <v1ztep@gmail.com>
2021-09-07 02:06:15 +0300

    Обернул в ifmain / def main

--- menu_upload.py
+++ menu_upload.py
@@ -7,26 +7,37 @@ from moltin import create_file
 from moltin import create_image_relationship
 from moltin import create_product
 
-load_dotenv()
+
+def main():
+    load_dotenv()
+    moltin_token = os.getenv('ELASTICPATH_CLIENT_ID')
+    moltin_secret = os.getenv('ELASTICPATH_CLIENT_SECRET')
+
+    with open("menu.json", "r", encoding="utf8") as file:
+        menu_json = file.read()
+    menu = json.loads(menu_json)
+
+    for pizza in menu:
+        name = pizza['name']
+        sku = pizza['id']
+        description = pizza['description']
+        price = pizza['price']
+        image_url = pizza['product_image']['url']
+        product = create_product(
+            moltin_token,
+            moltin_secret,
+            name,
+            sku,
+            description,
+            price
+        )
+        product_id = product['data']['id']
+        image = create_file(moltin_token, moltin_secret, image_url)
+        image_id = image['data']['id']
+        create_image_relationship(
+            moltin_token, moltin_secret, product_id, image_id
+        )
 
 
-moltin_token = os.getenv('ELASTICPATH_CLIENT_ID')
-moltin_secret = os.getenv('ELASTICPATH_CLIENT_SECRET')
-
-with open("menu.json", "r", encoding="utf8") as file:
-    menu_json = file.read()
-menu = json.loads(menu_json)
-
-for pizza in menu:
-    name = pizza['name']
-    sku = pizza['id']
-    description = pizza['description']
-    price = pizza['price']
-    image_url = pizza['product_image']['url']
-    product = create_product(moltin_token, moltin_secret, name,
-                             sku, description, price)
-    product_id = product['data']['id']
-    image = create_file(moltin_token, moltin_secret, image_url)
-    image_id = image['data']['id']
-    create_image_relationship(moltin_token, moltin_secret,
-                              product_id, image_id)
+if __name__ == '__main__':
+    main()
