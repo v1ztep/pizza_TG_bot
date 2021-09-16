2a854cdca7c76885ad55e4a2fef193162a040eb4
v1ztep <v1ztep@gmail.com>
2021-09-16 19:44:37 +0300

    Наполнил шаблон инфой из CMS

---
+++ facebook_contents.py
@@ -0,0 +1,58 @@
+import json
+import os
+
+import requests
+
+from moltin import get_products
+
+
+def send_menu(recipient_id):
+    moltin_token = os.environ["ELASTICPATH_CLIENT_ID"]
+    moltin_secret = os.environ["ELASTICPATH_CLIENT_SECRET"]
+    elements = get_elements(moltin_token, moltin_secret)
+    url = 'https://graph.facebook.com/v11.0/me/messages'
+    params = {"access_token": os.environ["FB_PAGE_ACCESS_TOKEN"]}
+    headers = {"Content-Type": "application/json"}
+    request_content = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": elements
+                }
+            }
+        }
+    })
+    response = requests.post(
+        url, params=params, headers=headers, data=request_content
+    )
+    print(response.json())
+    response.raise_for_status()
+    return response.json()
+
+
+def get_elements(moltin_token, moltin_secret):
+    user_current_page = 0
+    on_page = 5
+    products_per_page = get_products(
+        moltin_token,
+        moltin_secret,
+        page_offset=user_current_page * on_page,
+        limit_per_page=on_page
+    )
+    elements = []
+    for product in products_per_page['data']:
+        elements.append({
+            "title": product['name'],
+            "subtitle": product['description'],
+            "buttons": [{
+                    "type": "postback",
+                    "title": "Добавить в корзину",
+                    "payload": "DEVELOPER_DEFINED_PAYLOAD"
+                }]
+        })
+    return elements
