1c25662523ad051714a7792fbcd5559dceff1650
v1ztep <v1ztep@gmail.com>
2021-09-19 22:04:29 +0300

    Изменил имена категория на русские

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
                     # send_message(sender_id, message_text)
-                    send_menu(sender_id, 'main')
+                    send_menu(sender_id, 'Основные')
     return "ok", 200
 
 
