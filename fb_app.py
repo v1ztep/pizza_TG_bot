eec00686fa05589caa426933253df1b29ee797a5
v1ztep <v1ztep@gmail.com>
2021-09-28 18:24:36 +0300

    Переименовал переменную окружения FB_APP_VERIFY_TOKEN

--- fb_app.py
+++ fb_app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def verify():
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+        if not request.args.get("hub.verify_token") == os.environ["FB_APP_VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
