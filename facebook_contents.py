d6b47ce3b268afe5f8efa8bb49caa86e54b97734
v1ztep <v1ztep@gmail.com>
2021-09-28 19:03:41 +0300

    Убрал дубликат кода и привёл к одному формату кавычек

--- facebook_contents.py
+++ facebook_contents.py
@@ -7,29 +7,8 @@ from moltin import get_cart_items
 
 
 def send_menu(recipient_id, category, db):
-    elements = get_menu_elements(category, db)
-    url = 'https://graph.facebook.com/v11.0/me/messages'
-    params = {"access_token": os.environ["FB_PAGE_ACCESS_TOKEN"]}
-    headers = {"Content-Type": "application/json"}
-    request_content = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": elements
-                }
-            }
-        }
-    })
-    response = requests.post(
-        url, params=params, headers=headers, data=request_content
-    )
-    response.raise_for_status()
-    return response.json()
+    menu_elements = get_menu_elements(category, db)
+    send_message_template(recipient_id, menu_elements)
 
 
 def get_menu_elements(category, db):
@@ -51,7 +30,7 @@ def get_menu_elements(category, db):
                 image_url=product['image_url'],
                 subtitle=product['subtitle'],
                 buttons_payload={
-                    'Добавить в корзину': f"{product['id']} {product['title']}"
+                    'Добавить в корзину': f'{product["id"]} {product["title"]}'
                 }
             )
         )
@@ -68,31 +47,10 @@ def get_menu_elements(category, db):
 
 
 def send_cart(recipient_id):
-    moltin_token = os.environ["ELASTICPATH_CLIENT_ID"]
-    moltin_secret = os.environ["ELASTICPATH_CLIENT_SECRET"]
-    elements = get_cart_elements(moltin_token, moltin_secret, recipient_id)
-    url = 'https://graph.facebook.com/v11.0/me/messages'
-    params = {"access_token": os.environ["FB_PAGE_ACCESS_TOKEN"]}
-    headers = {"Content-Type": "application/json"}
-    request_content = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": elements
-                }
-            }
-        }
-    })
-    response = requests.post(
-        url, params=params, headers=headers, data=request_content
-    )
-    response.raise_for_status()
-    return response.json()
+    moltin_token = os.environ['ELASTICPATH_CLIENT_ID']
+    moltin_secret = os.environ['ELASTICPATH_CLIENT_SECRET']
+    cart_elements = get_cart_elements(moltin_token, moltin_secret, recipient_id)
+    send_message_template(recipient_id, cart_elements)
 
 
 def get_cart_elements(moltin_token, moltin_secret, recipient_id):
@@ -116,30 +74,55 @@ def get_cart_elements(moltin_token, moltin_secret, recipient_id):
         image_url = product['image']['href']
         cart_elements.append(
             get_generic_template(
-                title=f"{product['name']} ({product['quantity']} шт.)",
+                title=f'{product["name"]} ({product["quantity"]} шт.)',
                 image_url=image_url,
                 subtitle=product['description'],
                 buttons_payload={
-                    'Добавить ещё одну': f"{product['product_id']} {product['name']}",
-                    'Убрать из корзины': f"{product['id']} {product['name']}"
+                    'Добавить ещё одну': f'{product["product_id"]} {product["name"]}',
+                    'Убрать из корзины': f'{product["id"]} {product["name"]}'
                 }
             )
         )
     return cart_elements
 
 
+def send_message_template(recipient_id, elements):
+    url = 'https://graph.facebook.com/v11.0/me/messages'
+    params = {'access_token': os.environ['FB_PAGE_ACCESS_TOKEN']}
+    headers = {'Content-Type': 'application/json'}
+    request_content = json.dumps({
+        'recipient': {
+            'id': recipient_id
+        },
+        'message': {
+            'attachment': {
+                'type': 'template',
+                'payload': {
+                    'template_type': 'generic',
+                    'elements': elements
+                }
+            }
+        }
+    })
+    response = requests.post(
+        url, params=params, headers=headers, data=request_content
+    )
+    response.raise_for_status()
+    return response.json()
+
+
 def get_generic_template(title, image_url, subtitle, buttons_payload):
     buttons = []
     for button_title, payload in buttons_payload.items():
         buttons.append({
-            "type": "postback",
-            "title": button_title,
-            "payload": payload
+            'type': 'postback',
+            'title': button_title,
+            'payload': payload
         })
     template = {
-        "title": title,
-        "image_url": image_url,
-        "subtitle": subtitle,
-        "buttons": buttons
+        'title': title,
+        'image_url': image_url,
+        'subtitle': subtitle,
+        'buttons': buttons
     }
     return template
